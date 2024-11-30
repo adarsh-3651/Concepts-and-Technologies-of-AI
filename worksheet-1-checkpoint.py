4f66bfa87fe625c26b5adde91fc8ba53eaa5ab0f
Adarsh Kumar Mandal <sauravmandal365@gmail.com>
2024-11-30 20:23:26 +0545

    Add files via upload
    This is my worksheet solution for week q.

---
+++ worksheet-1-checkpoint.py
@@ -0,0 +1,11 @@
+import numpy as np
+#  Problem- 1: Array Creation:
+#  Complete the following Tasks:
+#  1. Initialize an empty array with size 2X2.
+#  2. Initialize an all one array with size 4X2.
+#  3. Return a new array of given shape and type, filled with fill value.{Hint: np.full}
+#  4. Return a new array of zeros with same shape and type as a given array.{Hint: np.zeros like}
+#  5. Return a new array of ones with same shape and type as a given array.{Hint: np.ones like}
+#  6. For an existing list new_list = [1,2,3,4] convert to an numpy array.{Hint: np.array()}
+empty_array=np.empty((2,2))
+empty_array
